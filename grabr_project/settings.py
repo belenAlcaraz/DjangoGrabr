13228d1dd47ad80fbeadc5a49ed164c1ba7b28b1
Belen Alcaraz <mariabelenalcaraz9919@gmail.com>
2023-12-01 01:47:30 -0300

    Actualizar configuración con nuevas aplicaciones

--- grabr_project/settings.py
+++ grabr_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'delivery.apps.DeliveryConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
