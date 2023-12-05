a5d03f52a1c39dec09f062bd88aa557ab98b336a
Belen Alcaraz <mariabelenalcaraz9919@gmail.com>
2023-12-05 01:50:27 -0300

    Registro de modelos en el admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Comprador,Viajero, Ubicacion
 
-# Register your models here.
+admin.site.register(Ubicacion)
+admin.site.register(User)
+admin.site.register(Comprador)
+admin.site.register(Viajero)
+
--- delivery/admin.py
+++ delivery/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Producto, OfertaDeEntrega
 
-# Register your models here.
+admin.site.register(Producto)
+admin.site.register(OfertaDeEntrega)
+
