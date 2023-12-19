e7faeb5c6558de5f932ed6c77b7bd5e842300c5d
Belen Alcaraz <mariabelenalcaraz9919@gmail.com>
2023-12-19 19:34:05 -0300

    actualizacion en archivos  html

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from accounts.views import UsuarioRegistroView
+from accounts.views import UsuarioRegistroView, ViajeroPerfilView, CompradorPerfilView
 
 
 urlpatterns = [
    path('registro/',UsuarioRegistroView.as_view(),name='Registro'),
+   path('viajero/<int:pk>/', ViajeroPerfilView.as_view(), name='Viajero_perfil'),
+   path('comprador/<int:pk>/', CompradorPerfilView.as_view(), name='Comprador_perfil'),
    
 
 ]
--- delivery/views.py
+++ delivery/views.py
@@ -1,9 +1,9 @@
-from django.forms.models import BaseModelForm
-from django.urls import reverse_lazy
-from django.shortcuts import render,redirect
+from django.forms.models import ModelForm
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView,CreateView, DetailView
 from .models import Producto, OfertaDeEntrega
-from accounts.models import Comprador
 from .forms import ProductoForm, OfertaDeEntregaForm
 
 class InicioView(ListView):
@@ -17,7 +17,7 @@ class ComprarProductoView(CreateView):
     template_name = 'comprar.html'
     success_url = reverse_lazy('Lista_producto')
 
-    def form_valid(self, form: BaseModelForm):
+    def form_valid(self, form: ModelForm):
         form.instance.usuario = self.request.user.comprador
         return super().form_valid(form)
 
@@ -33,33 +33,89 @@ class DetalleProductoView(DetailView):
         context['ofertas'] = OfertaDeEntrega.objects.filter(producto=producto)
         return context
 
-
 class ListaProductosView(ListView):
     model = Producto
     template_name = 'lista_productos.html'
     context_object_name = 'productos'
+    ordering = ['-id']
 
 class OfertaView(CreateView):
     model =  OfertaDeEntrega
     form_class = OfertaDeEntregaForm
     template_name = 'hacer_oferta_entrega.html'
+    success_url = reverse_lazy('Pedidos_viajero')
 
-def crear_oferta(request, producto_id):
-        producto = Producto.objects.get(pk=producto_id)
+    def form_valid(self, form):
+        producto_pk = self.kwargs['pk']
+        producto = get_object_or_404(Producto, pk=producto_pk)
+        form.instance.producto = producto
+        form.instance.viajero = self.request.user.viajero
+        self.producto_pk = producto_pk
+        return super().form_valid(form)
+   
+class PedidosViajeroView(ListView):
+    model = Producto
+    template_name = 'pedidos_viajero.html'
+    context_object_name = 'productos'
+  
+class OfertasViajeroView(ListView):
+    model= Producto
+    template_name = 'tus_ofertas.html'
+    context_object_name = 'productos'
+    
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        viajero = self.request.user.viajero
+        context['viajero'] = viajero
 
-        if request.method == 'POST':
-            form = OfertaDeEntregaForm(request.POST)
-            if form.is_valid():
-                oferta = form.save(commit=False)
-                oferta.producto = producto
-                oferta.viajero = request.user 
-                oferta.save()           
+        ofertas = OfertaDeEntrega.objects.filter(viajero=viajero)
 
-                producto.estado = 'en proceso'
-                producto.save()
+        ofertas_pendientes = ofertas.filter(estado='pendiente')
+        ofertas_aceptadas = ofertas.filter(estado='aceptado')
+        ofertas_en_proceso = ofertas.filter(estado='en proceso')
+        ofertas_completadas = ofertas.filter(estado='entregado')
+        ofertas_canceladas = ofertas.filter(estado='cancelado')
 
-                return redirect('detalle_producto', producto_id=producto_id)
-        else:
-            form = OfertaDeEntregaForm()
+        context['ofertas_pendientes'] = ofertas_pendientes
+        context['ofertas_aceptadas'] = ofertas_aceptadas
+        context['ofertas_en_proceso'] = ofertas_en_proceso
+        context['ofertas_completadas'] = ofertas_completadas
+        context['ofertas_canceladas'] = ofertas_canceladas
 
-        return render(request, 'Inicio', {'form': form, 'producto': producto})
+        return context
+
+class CompraDetalleViajero(DetailView):
+    model = Producto
+    template_name = 'compra_detalle_viajero.html'
+
+def aceptar_oferta(request, oferta_id):
+    oferta = get_object_or_404(OfertaDeEntrega, id=oferta_id)
+    oferta.estado = 'aceptado'
+    oferta.save()
+    producto = oferta.producto
+    producto.estado = 'en proceso'  
+    producto.save()
+    detalle_producto_url = reverse('Detalle_producto', kwargs={'pk': oferta.producto.pk})
+    return HttpResponseRedirect(detalle_producto_url)
+
+def rechazar_oferta(request, oferta_id):
+    oferta = get_object_or_404(OfertaDeEntrega, id=oferta_id)
+    oferta.estado = 'cancelado'
+    oferta.save()
+    producto = oferta.producto
+    producto.estado = 'cancelado'  
+    producto.save()
+    detalle_producto_url = reverse('Detalle_producto', kwargs={'pk': oferta.producto.pk})
+    return HttpResponseRedirect(detalle_producto_url)
+
+def confirmar_recibido(request, oferta_id):
+    oferta = get_object_or_404(OfertaDeEntrega, id=oferta_id)
+    oferta.estado = 'entregado'
+    oferta.save()
+    producto = oferta.producto
+    producto.estado = 'completado'  
+    producto.save()
+
+    detalle_producto_url = reverse('Detalle_producto', kwargs={'pk': oferta.producto.pk})
+    return HttpResponseRedirect(detalle_producto_url)
